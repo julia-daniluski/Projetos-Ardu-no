16dd75947b20266fcc61bc1ea94b17dd73ad1187
Julia Daniluski <julia.o.daniluski@gmail.com>
2024-08-05 15:21:25 -0300

    comentarios add

--- piscarleds/piscar_leds/piscarleds/piscarleds.ino
+++ piscarleds/piscar_leds/piscarleds/piscarleds.ino
@@ -1,14 +1,16 @@
+//c++
+// programa que envia sinal alto para a saida 6, aguarda 2s e envia sinal baixo para saida 6
+
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode (6, OUTPUT);
-
-}
+  pinMode (6, OUTPUT); //declarar portas
+  }
 
 void loop() {
   // put your main code here, to run repeatedly:
- digitalWrite (6, HIGH);
- delay (2000);
- digitalWrite (6, LOW);
+ digitalWrite (6, HIGH); // acende a led 
+ delay (2000) // tempo que vai esperar
+ digitalWrite (6, LOW); // apaga a led
  delay (2000);
 }
